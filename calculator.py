3f326d5ead47e27fec29a75239a026d15a7e0e2f
weez98 <96293490+weez98@users.noreply.github.com>
2023-02-05 21:16:06 +0300

    Update and rename calculator.cpp to calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def calculator():
+    return '''
+        <form action="/" method="post">
+            <input type="text" name="expression">
+            <input type="submit" value="Calculate">
+        </form>
+    '''
+
+@app.route('/', methods=['POST'])
+def evaluate_expression():
+    expression = request.form['expression']
+    try:
+        result = eval(expression)
+        return 'Result: {}'.format(result)
+    except:
+        return 'Invalid expression'
+
+if __name__ == '__main__':
+    app.run()
